9a2840a71540a8fc963c0e72e5b0e8cd3d2bd751
DaNuwa <yhan7788@gmail.com>
2019-11-24 21:24:45 +0530

    Updates

---
+++ precache-manifest.306883b27dc2f2331131b524b339d9cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1138f66978bd771d3dd3a18453e4326",
+    "url": "/Christmas-ToDoList/index.html"
+  },
+  {
+    "revision": "d19bfcfd7b7cd4e5392c",
+    "url": "/Christmas-ToDoList/static/css/main.8e8a2d5c.chunk.css"
+  },
+  {
+    "revision": "45b71a4e2dc072d89d4e",
+    "url": "/Christmas-ToDoList/static/js/2.3c9d194c.chunk.js"
+  },
+  {
+    "revision": "d19bfcfd7b7cd4e5392c",
+    "url": "/Christmas-ToDoList/static/js/main.c1d2bbd9.chunk.js"
+  },
+  {
+    "revision": "4938faa1218ce861da0d",
+    "url": "/Christmas-ToDoList/static/js/runtime-main.77256892.js"
+  }
+]);
